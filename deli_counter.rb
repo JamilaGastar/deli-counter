aacf05c23ab2f3627ac6b0e71be8538592493d08
jamilagastar <jamilagastar@gmail.com>
2022-08-16 21:11:27 +1000

    partially completed lab

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    katz_deli.each.with_index(1).collect {|name, number| puts "Welcome, #{name}. You are number #{number} in line."}
+end
+
+take_a_number(katz_deli, "Jamila")
+take_a_number(katz_deli, "Eva")
+
+def line(katz_deli)
+    if katz_deli == []
+        puts "The line is currently empty."
+    else 
+        katz_deli.each.with_index(1) do |name, index| "#{index}. #{name}"
+                                     end
+    end
+end
+
+line(katz_deli)
+
